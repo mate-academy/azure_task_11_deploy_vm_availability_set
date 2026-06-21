113515f4b062871f64021c8a05cbd1791e1b2945
Eugene Ermakov <eugeniy696@gmail.com>
2026-06-21 19:49:05 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "ukwest"
 $resourceGroupName = "mate-azure-task-11"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,10 +6,10 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIfVhFZjYAhjVr+8an2+m98NzofTYHwe0M9rtezqgEAZ1qVHhJisVJXaS3u3Yq0O9/PK4fm/rA7qsL3A3OXOwE4Cihwvt340wbXC96NbcitJfykYDF8qrcYxzklXG/dvbZgPPyO0mwMHnlMKae8TqswEdmQ8l4cMFH1OQDkhwGI5PtCAFTDohqQoNZ3I4OLarD78O6lhX97DEBnQchPqp3IJwionPCIhM0kPsTj0dT43uJ4zAa6kMuTztDGknJlkREqtf11+g0UMiwsN1xS6Cp3u7jTid6Wn2RUNOgRfXhPDaTvlw3YVVQPTfgslD0CbSlxPTqfM/KVMtx6Vr1IJljXCsnUlrhPETkfQ/9IHGFzG+S7tdI7j482ANHltvH7+y0xMxR3zjm30SLAi03VVvMkvq8Gqqrlr4mfkRccx0vAamsMxlnjBtQDp/n42ihpJG6LlzeQg2kEMKVGA8kgdpfgaxdrY3KE91YzGRBPbl51Jp22EziL4q1BO3z5oqWdw0= generated-by-azure"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$vmSize = "Standard_D2s_v3"
 $availabilitySetName = "mateavalset"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
@@ -25,15 +25,24 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-for (($zone = 1); ($zone -le 2); ($zone++) ) {
+New-AzAvailabilitySet `
+    -Location $location `
+    -Name $availabilitySetName `
+    -ResourceGroupName $resourceGroupName `
+    -Sku aligned `
+    -PlatformFaultDomainCount 2 `
+    -PlatformUpdateDomainCount 2
+
+for (($i = 1); ($i -le 2); ($i++) ) {
     New-AzVm `
     -ResourceGroupName $resourceGroupName `
-    -Name "$vmName-$zone" `
+    -Name "$vmName-$i" `
     -Location $location `
     -image $vmImage `
     -size $vmSize `
     -SubnetName $subnetName `
     -VirtualNetworkName $virtualNetworkName `
     -SecurityGroupName $networkSecurityGroupName `
-    -SshKeyName $sshKeyName -Zone $zone
+    -SshKeyName $sshKeyName `
+    -AvailabilitySetName $availabilitySetName
 }
