3a7c6e614b9d083c06237577e6e6774c5e407fad
Alexey <alexey.zhuravel2004@gmail.com>
2026-08-20 13:41:49 +0200

    Sol1

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "polandcentral"
 $resourceGroupName = "mate-azure-task-11"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -9,7 +9,7 @@ $sshKeyName = "linuxboxsshkey"
 $sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$vmSize = "Standard_B2ats_v2"
 $availabilitySetName = "mateavalset"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
@@ -25,15 +25,30 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-for (($zone = 1); ($zone -le 2); ($zone++) ) {
+$availabilitySetParams = @{
+    Location                  = $location
+    Name                      = $availabilitySetName
+    ResourceGroupName         = $resourceGroupName
+    Sku                       = 'Aligned'
+    PlatformFaultDomainCount  = 2
+    PlatformUpdateDomainCount = 2
+}
+
+New-AzAvailabilitySet @availabilitySetParams
+
+$credentials = Get-Credential -UserName "alexey"
+
+for (($vmNumber = 1); ($vmNumber -le 2); ($vmNumber++) ) {
     New-AzVm `
     -ResourceGroupName $resourceGroupName `
-    -Name "$vmName-$zone" `
+    -Name "$vmName-$vmNumber" `
     -Location $location `
     -image $vmImage `
     -size $vmSize `
     -SubnetName $subnetName `
     -VirtualNetworkName $virtualNetworkName `
     -SecurityGroupName $networkSecurityGroupName `
-    -SshKeyName $sshKeyName -Zone $zone
+    -SshKeyName $sshKeyName `
+    -Credential $credentials `
+    -AvailabilitySetName $availabilitySetName `
 }
